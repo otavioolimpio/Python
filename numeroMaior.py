cbb546437a59433947076e317d40572160a6d348
Otávio Olímpio <otavioolimpio@outlook.com>
2024-06-17 17:32:27 -0300

    Add files via upload

---
+++ numeroMaior.py
@@ -0,0 +1,11 @@
+#Esse programa destina-se a verificação de qual número é maior em uma entrada com dois valores.
+
+#Recebimento dos dados
+primeiroNumero = int(input("Insira o primeiro valor:"))
+segundoNumero = int(input("Insira o segundo valor:"))
+
+#Tratando os dados e imprimindo o resultado
+if primeiroNumero > segundoNumero:
+    print("O número", primeiroNumero, "é maior que o número", segundoNumero)
+else:
+    print("O número", segundoNumero, "é maior que o número", primeiroNumero)
