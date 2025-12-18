a7bf9c2a00b22b0fdd61fe008059af8d327c8ed4
Otávio Olímpio <otavioolimpio@outlook.com>
2025-12-17 23:42:17 -0300

    TiposDados
    Tipos de dados em Python

---
+++ Bootcamp/tiposDados.py
@@ -0,0 +1,19 @@
+print("Verdadeiro" if True else "Falso")
+
+"""
+int()
+float()
+str()
+bool()
+list()
+dict()
+set()   
+tuple()
+complex()   
+bytes()
+bytearray() 
+memoryview()
+range(10)
+frozenset()
+type(None)
+"""
