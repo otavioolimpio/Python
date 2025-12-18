4bfbf30de882ac39bf96e556ca68e75b88423289
Otávio Olímpio <otavioolimpio@outlook.com>
2025-12-18 09:46:50 -0300

    File de variaveis e constante
    Desvendando variáveis e constantes

---
+++ Bootcamp/variaveisConstantes.py
@@ -0,0 +1,9 @@
+nome = ("Otavio") # Variável
+idade = 26 # Variável
+nome, idade = "Olimpio", 30 # Reatribuição de variáveis
+limite_saque = 500.00 # Variável com snake_case
+ESTADOS = ["SP", "RJ", "MG", "ES"] # Constante com letras maiúsculas
+
+print(nome, idade)
+print(limite_saque)
+print(ESTADOS)
