f1591d4a11f0dd558a87733224b24d93e193b53e
Otávio Olímpio <otavioolimpio@outlook.com>
2025-12-23 09:50:20 -0300

    Conversao de Tipos

---
+++ Bootcamp/conversaoTipos.py
@@ -0,0 +1,19 @@
+print(int("123"))        # Converte string para inteiro 
+print(float("123.45"))   # Converte string para float
+print(str(123))         # Converte inteiro para string
+print(str(123.45))      # Converte float para string
+print(bool(1))          # Converte inteiro para booleano (True)
+print(bool(0))          # Converte inteiro para booleano (False)
+print(bool(""))         # Converte string vazia para booleano (False)
+print(bool("Hello"))    # Converte string não vazia para booleano (True)
+print(list((1, 2, 3)))  # Converte tupla para lista
+print(tuple([1, 2, 3])) # Converte lista para tupla
+
+print(type(123))        # Verifica o tipo do valor (int)
+print(type(123.45))     # Verifica o tipo do valor (float)
+print(type("Hello"))    # Verifica o tipo do valor (str)
+print(type([1, 2, 3]))  # Verifica o tipo do valor (list)
+print(type((1, 2, 3)))  # Verifica o tipo do valor (tuple)
+
+print(100/3)       # Divisão normal (float)
+print(100//3)      # Divisão inteira (int)
---
+++ Bootcamp/print_input.py
@@ -0,0 +1,3 @@
+nome = input("Informe seu nome: ") # Solicita o nome do usuário
+idade = input("Informe sua idade: ") # Solicita a idade do usuário
+print("Olá,", nome, "você tem", idade, "anos.", sep=" ", end="...") # Adiciona reticências ao final da frase
